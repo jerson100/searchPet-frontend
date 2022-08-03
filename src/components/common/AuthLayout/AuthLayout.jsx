e217d0b8fdab05379eec8627fd8eaa9294fc2526
jerson100 <juamkoo@gmail.com>
2022-08-02 21:07:31 -0500

    Agregando proptypes

--- src/components/common/AuthLayout/AuthLayout.jsx
+++ src/components/common/AuthLayout/AuthLayout.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import Box from "@mui/material/Box";
 // import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import PropTypes from "prop-types";
 
-const AuthLayout = ({ children }) => {
+const AuthLayout = ({ children, maxWidth }) => {
   return (
     <Box
       display={"flex"}
@@ -11,11 +12,19 @@ const AuthLayout = ({ children }) => {
       alignItems={"center"}
       minHeight="100vh"
     >
-      <Box maxWidth={"1000px"} width="100%" style={{ padding: "1rem" }}>
+      <Box maxWidth={maxWidth} width="100%" style={{ padding: "1rem" }}>
         <Paper elevation={5}>{children}</Paper>
       </Box>
     </Box>
   );
+};
+
+AuthLayout.propTypes = {
+  maxWidth: PropTypes.string,
+};
+
+AuthLayout.defaultProps = {
+  maxWidth: "1000px",
 };
 
 export default AuthLayout;
