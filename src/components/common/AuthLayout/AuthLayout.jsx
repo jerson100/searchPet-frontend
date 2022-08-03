833fd4a0f552bcfb6d611a0e5c05b06ce5debb13
jerson100 <juamkoo@gmail.com>
2022-08-02 20:49:22 -0500

    Generando el componente authlayout para reutilizarlo en el register tambi{en

---
+++ src/components/common/AuthLayout/AuthLayout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Box from "@mui/material/Box";
+// import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+
+const AuthLayout = ({ children }) => {
+  return (
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      minHeight="100vh"
+    >
+      <Box maxWidth={"1000px"} width="100%">
+        <Paper elevation={5}>{children}</Paper>
+      </Box>
+    </Box>
+  );
+};
+
+export default AuthLayout;
