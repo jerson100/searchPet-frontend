1342b203b8044c8103d816b83cf6c86db8843bd2
jerson100 <juamkoo@gmail.com>
2022-08-18 11:53:45 -0500

    En mobile no se mostraba el button logout por ende necesitamos agregarle margin bottom

--- src/components/common/Header/components/MobileMenu/components/Logout/Logout.jsx
+++ src/components/common/Header/components/MobileMenu/components/Logout/Logout.jsx
@@ -14,7 +14,7 @@ const Logout = () => {
   return (
     <>
       <Divider component="li" />
-      <ListItemButton component="li" onClick={logout}>
+      <ListItemButton component="li" onClick={logout} sx={{ mb: 5 }}>
         <ListItemIcon>
           <LogoutIcon />
         </ListItemIcon>
