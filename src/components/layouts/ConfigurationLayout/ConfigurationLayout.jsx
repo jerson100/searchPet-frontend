25fa6a1c8cb49166c47b27075bd5086e281ade84
jerson100 <juamkoo@gmail.com>
2022-08-18 11:31:14 -0500

    Solucionando el redireccionamiento si es que no inicia sesión al acceder a una ruta privada

--- src/components/layouts/ConfigurationLayout/ConfigurationLayout.jsx
+++ src/components/layouts/ConfigurationLayout/ConfigurationLayout.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthContext } from "../../../hooks/useAuthContext";
 
 const ConfigurationLayout = () => {
+  const { previousLoading, isLogued } = useAuthContext();
+  if (previousLoading) return null;
+  if (!isLogued) return <Navigate to={"/login"} />;
   return <div>{<Outlet />}</div>;
 };
 
