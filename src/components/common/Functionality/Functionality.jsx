68b04515ed8705c64f201dc85dad0a1a004ecdb1
jerson100 <juamkoo@gmail.com>
2022-08-23 05:07:10 -0500

    Cambiando el tam de las imágenes de las funcionalidades en la v desktop

--- src/components/common/Functionality/Functionality.jsx
+++ src/components/common/Functionality/Functionality.jsx
@@ -12,7 +12,7 @@ const Functionality = ({ svg, text, viewBox = "0 0 400 300" }) => {
         sx={{
           width: { xs: "100%" },
           marginBottom: "1rem",
-          height: { xs: "100px", md: "150px", lg: "200px" },
+          height: { xs: "100px", md: "150px", lg: "160px" },
         }}
       />
       <Typography variant="body1" textAlign={"center"}>
