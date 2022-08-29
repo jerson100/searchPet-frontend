fec8bd0d341569933e19cc77a0247f0a595c325e
jerson100 <juamkoo@gmail.com>
2022-08-29 04:12:55 -0500

    Agregando nuevo componenten para manejar los input select

---
+++ src/components/common/JeSelectError/JeSelectError.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import PropTypes from "prop-types";
+import { MenuItem, Select } from "@mui/material";
+
+const JeSelectError = ({
+  required,
+  fullWidth,
+  variant,
+  name,
+  size,
+  value,
+  handleChange,
+  error,
+  errorMessage,
+  inputLabel,
+  selectItems,
+}) => {
+  return (
+    <FormControl
+      required={required}
+      fullWidth={fullWidth}
+      variant={variant}
+      margin="normal"
+    >
+      <Select
+        labelId={name}
+        id={name}
+        size={size}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        error={error}
+        label={inputLabel}
+      >
+        <MenuItem value="">
+          <em>Ninguno</em>
+        </MenuItem>
+        {selectItems?.map(({ name, value }) => (
+          <MenuItem key={value} value={value}>
+            {name}
+          </MenuItem>
+        ))}
+      </Select>
+      <InputLabel size="small" htmlFor={name}>
+        {inputLabel}
+      </InputLabel>
+      <FormHelperText>{errorMessage}</FormHelperText>
+    </FormControl>
+  );
+};
+
+JeSelectError.propTypes = {
+  required: PropTypes.bool,
+  fullWidth: PropTypes.bool,
+  variant: PropTypes.string,
+  name: PropTypes.string,
+  size: PropTypes.string,
+  value: PropTypes.string,
+  handleChange: PropTypes.func,
+  error: PropTypes.bool,
+  errorMessage: PropTypes.string,
+  inputLabel: PropTypes.string,
+  selectItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      value: PropTypes.string,
+    })
+  ),
+};
+
+JeSelectError.defaultProps = {
+  required: false,
+  fullWidth: false,
+  variant: "outlined",
+  name: "",
+  size: "small",
+  value: "",
+  handleChange: () => {},
+  error: false,
+  errorMessage: "",
+  inputLabel: "",
+};
+
+export default React.memo(JeSelectError);
