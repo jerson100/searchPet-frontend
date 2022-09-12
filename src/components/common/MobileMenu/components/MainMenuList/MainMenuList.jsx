628bb5db7e640bcf8c1bfebba5bcb34335318864
jerson100 <juamkoo@gmail.com>
2022-09-12 06:18:20 -0500

    Agregamos nuevo item en la lista, en este caso se muestra siempre y cuando tenga la sesión iniciada

--- src/components/common/MobileMenu/components/MainMenuList/MainMenuList.jsx
+++ src/components/common/MobileMenu/components/MainMenuList/MainMenuList.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
 import PeopleIcon from "@mui/icons-material/People";
 import HomeIcon from "@mui/icons-material/Home";
 import HelpIcon from "@mui/icons-material/Help";
 import PetsIcon from "@mui/icons-material/Pets";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import FlagIcon from "@mui/icons-material/Flag";
+import AddPetIcon from "@mui/icons-material/Add";
+import { useAuthContext } from "../../../../../hooks/useAuthContext";
 
 const MainMenuList = () => {
   return (
@@ -23,18 +30,7 @@ const MainMenuList = () => {
           </ListItemIcon>
           <ListItemText primary="Home" />
         </ListItemButton>
-        <ListItemButton to="/pets" component={Link}>
-          <ListItemIcon>
-            <PetsIcon />
-          </ListItemIcon>
-          <ListItemText primary="Mascotas" />
-        </ListItemButton>
-        <ListItemButton to="/lostPet" component={Link}>
-          <ListItemIcon>
-            <PetsIcon />
-          </ListItemIcon>
-          <ListItemText primary="Mascotas perdidas" />
-        </ListItemButton>
+        <PetListItem />
         <ListItemButton to="/we" component={Link}>
           <ListItemIcon>
             <HelpIcon />
@@ -49,6 +45,50 @@ const MainMenuList = () => {
         </ListItemButton>
       </List>
     </li>
+  );
+};
+
+const PetListItem = () => {
+  const [openPet, setOpenPets] = useState(false);
+  const { user } = useAuthContext();
+
+  const handleClick = () => {
+    setOpenPets((prev) => !prev);
+  };
+  return (
+    <>
+      <ListItemButton onClick={handleClick}>
+        <ListItemIcon>
+          <PetsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Mascotas" />
+        {openPet ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={openPet} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItemButton to="/pets" component={Link} sx={{ pl: 4 }}>
+            <ListItemIcon>
+              <VisibilityIcon />
+            </ListItemIcon>
+            <ListItemText primary="Ver mascotas" />
+          </ListItemButton>
+          <ListItemButton to="/lostPet" component={Link} sx={{ pl: 4 }}>
+            <ListItemIcon>
+              <FlagIcon />
+            </ListItemIcon>
+            <ListItemText primary="Mascotas perdidas" />
+          </ListItemButton>
+          {user && (
+            <ListItemButton to="/my-pet/add" component={Link} sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <AddPetIcon />
+              </ListItemIcon>
+              <ListItemText primary="Agregar mascota" />
+            </ListItemButton>
+          )}
+        </List>
+      </Collapse>
+    </>
   );
 };
 
