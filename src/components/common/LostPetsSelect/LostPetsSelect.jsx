125640cf83d035ab6ad86bd057c3605020205766
jerson100 <juamkoo@gmail.com>
2022-09-27 01:59:10 -0500

    Adaptando el componente de selecci{on de mui con formik

--- src/components/common/LostPetsSelect/LostPetsSelect.jsx
+++ src/components/common/LostPetsSelect/LostPetsSelect.jsx
@@ -1,34 +1,116 @@
-import { Avatar, Grid } from "@mui/material";
 import React from "react";
-import { useState } from "react";
-import JeGroupSelection from "../JeGroupSelection";
+import {
+  Avatar,
+  FormControl,
+  Grid,
+  OutlinedInput,
+  InputLabel,
+  MenuItem,
+  Select,
+  Chip,
+  Box,
+  FormHelperText,
+} from "@mui/material";
+import PropTypes from "prop-types";
+import { ErrorMessage, Field } from "formik";
 
-const LostPetsSelect = () => {
-  const [pets, setpets] = useState([]);
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const LostPetsSelect = ({
+  required,
+  fullWidth,
+  variant,
+  name,
+  size,
+  inputLabel,
+  selectItems,
+  multiple = true,
+  error,
+}) => {
   return (
-    <JeGroupSelection
-      multiple
-      direction={"column"}
-      value={pets}
-      onChange={setpets}
+    <FormControl
+      required={required}
+      fullWidth={fullWidth}
+      variant={variant}
+      margin="normal"
     >
-      <JeGroupSelection.Item value={1}>
-        <LostPetItem name="jerson" urlImageProfile={""} />
-      </JeGroupSelection.Item>
-      <JeGroupSelection.Item value={2}>
-        <LostPetItem name="jerson" urlImageProfile={""} />
-      </JeGroupSelection.Item>
-      <JeGroupSelection.Item value={3}>
-        <LostPetItem name="jerson" urlImageProfile={""} />
-      </JeGroupSelection.Item>
-      <JeGroupSelection.Item value={4}>
-        <LostPetItem name="jerson" urlImageProfile={""} />
-      </JeGroupSelection.Item>
-      <JeGroupSelection.Item value={5}>
-        <LostPetItem name="jerson" urlImageProfile={""} />
-      </JeGroupSelection.Item>
-    </JeGroupSelection>
+      <Field
+        as={Select}
+        labelId={name}
+        id={name}
+        size={size}
+        name={name}
+        error={error}
+        label={inputLabel}
+        multiple={multiple}
+        renderValue={(selected) => (
+          <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+            {selected.map((value) => (
+              <Chip
+                key={value}
+                label={value}
+                avatar={<Avatar title="Jerson" />}
+              />
+            ))}
+          </Box>
+        )}
+        MenuProps={MenuProps}
+        sx={{
+          backgroundColor: (theme) => {
+            return theme.palette.background.paper;
+          },
+        }}
+      >
+        {selectItems.map(({ name }) => (
+          <MenuItem key={name} value={name}>
+            <LostPetItem key={name} name={name} urlImageProfile="" />
+          </MenuItem>
+        ))}
+      </Field>
+      <InputLabel size="small" htmlFor={name}>
+        {inputLabel}
+      </InputLabel>
+      <ErrorMessage
+        name={name}
+        render={(msg) => <FormHelperText>{msg}</FormHelperText>}
+      />
+    </FormControl>
   );
+};
+
+LostPetsSelect.propTypes = {
+  required: PropTypes.bool,
+  fullWidth: PropTypes.bool,
+  variant: PropTypes.string,
+  name: PropTypes.string,
+  size: PropTypes.string,
+  inputLabel: PropTypes.string,
+  selectItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      value: PropTypes.string,
+    })
+  ),
+  error: PropTypes.bool,
+};
+
+LostPetsSelect.defaultProps = {
+  required: false,
+  fullWidth: false,
+  variant: "outlined",
+  name: "",
+  size: "small",
+  inputLabel: "",
+  error: false,
 };
 
 const LostPetItem = React.memo(({ name, urlImageProfile }) => {
@@ -38,7 +120,7 @@ const LostPetItem = React.memo(({ name, urlImageProfile }) => {
         <Avatar src={urlImageProfile} title={name} />
       </Grid>
       <Grid item flexGrow={1}>
-        Nombre de la mascota
+        {name}
       </Grid>
     </Grid>
   );
