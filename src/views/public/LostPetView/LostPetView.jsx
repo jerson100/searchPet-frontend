2f67b64db3cd27334819662a03b0f36afac5c5f1
jerson100 <juamkoo@gmail.com>
2022-10-04 05:55:34 -0500

    Obteniendo el registro de la mascota que viene por el parámetro y diviendo en secciones en la vista de lostpet

--- src/views/public/LostPetView/LostPetView.jsx
+++ src/views/public/LostPetView/LostPetView.jsx
@@ -1,7 +1,59 @@
+import useAxios from "axios-hooks";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { Grid, Typography } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import LostPetItem from "../../../components/common/LostPetItem";
 
 const LostPetView = () => {
-  return <div>LostPetView</div>;
+  const params = useParams();
+  const [{ loading, data: lostPet, error }] = useAxios({
+    url: `/lostpet/${params.idLostPet}`,
+    method: "GET",
+  });
+  return (
+    <Container
+      sx={{
+        p: { xs: 0, sm: 3 },
+        pt: { xs: 2, sm: 3 },
+        pb: { xs: 2, sm: 3 },
+      }}
+    >
+      {loading ? (
+        <Typography paragraph>Cargando...</Typography>
+      ) : (
+        <>
+          <Grid container spacing={2} mb={2}>
+            <Grid item xs={12} md={5} lg={4}>
+              <LostPetItem
+                _id={lostPet._id}
+                createdAt={lostPet.createdAt}
+                description={lostPet.description}
+                images={lostPet.images}
+                located={lostPet.located}
+                pets={lostPet.pets}
+                user={lostPet.user}
+              />
+            </Grid>
+            <Grid item xs={12} md={7} lg={8}>
+              <Box
+                bgcolor={"background.paper"}
+                border="solid 1px"
+                borderColor={"divider"}
+                height="100%"
+              ></Box>
+            </Grid>
+          </Grid>
+          <Box
+            bgcolor={"background.paper"}
+            border="solid 1px"
+            borderColor={"divider"}
+            height="200px"
+          ></Box>
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default LostPetView;
