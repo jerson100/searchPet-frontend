2b5a903747d5128915f76410f0179c902b107e20
jerson100 <juamkoo@gmail.com>
2022-10-13 11:34:35 -0500

    Ya no es necesario los estilos personalizados

--- src/components/common/JeSection/jeSection.style.js
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-import styled from "@mui/system/styled";
-
-const TitleStyle = styled(Typography)`
-  text-align: center;
-  margin-bottom: 3rem;
-`;
-
-export { TitleStyle };
