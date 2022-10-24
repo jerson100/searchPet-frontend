27acc72f84b49585585cc0792ce2dc4da8affce9
jerson100 <juamkoo@gmail.com>
2022-10-24 11:19:11 -0500

    Obtención de mascotas del usuario x

--- src/views/public/UserProfileView/views/UserPets/UserPets.jsx
+++ src/views/public/UserProfileView/views/UserPets/UserPets.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import JeSection from "../../../../../components/common/JeSection";
+import PetList from "../../../../../components/common/PetList";
+import { usePets } from "../../../../../hooks/usePets";
 
 const UserPets = () => {
+  const params = useParams();
+  const {
+    isNext,
+    loading: loadingGetPets,
+    pets,
+    handleNext,
+    page,
+  } = usePets(`/users/${params.idUser}/pets`, null, 1, 5);
   return (
     <>
       <JeSection.Title textAlign="left" variant="h5">
         Mascotas
       </JeSection.Title>
+      {page == 1 && loadingGetPets ? (
+        <PetList.Loading />
+      ) : (
+        <PetList
+          component="div"
+          bordered={false}
+          pets={pets}
+          loading={loadingGetPets}
+          page={page}
+          isNext={isNext}
+          handleNext={handleNext}
+        />
+      )}
     </>
   );
 };
