2470e5ad1c91ea0bf46b2f1a2e6d1f59a9146a15
jerson100 <juamkoo@gmail.com>
2022-11-01 16:23:26 -0500

    Se hace más pequeño al contenedor de la imegen de portada del login

--- src/views/public/LoginView/LoginView.jsx
+++ src/views/public/LoginView/LoginView.jsx
@@ -18,11 +18,26 @@ const LoginView = () => {
       </Helmet>
       <AuthLayout>
         <Grid container>
-          <Grid item xs={12} md={7}>
+          <Grid
+            item
+            xs={12}
+            md={7}
+            sx={{
+              position: "relative",
+              height: { xs: "157px", md: "initial" },
+            }}
+          >
             <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
               component="img"
               src="https://www.elespectador.com/resizer/6_cZmkvFc1dqaJeNlklnGrtmbrI=/920x613/filters:format(jpeg)/cloudfront-us-east-1.images.arcpublishing.com/elespectador/V5RZA43Z7FEQVM6ASB46MPVXTI.jpg"
-              style={{ objectFit: "cover", height: "100%", width: "100%" }}
             ></Box>
           </Grid>
           <Grid item xs={12} md={5}>
