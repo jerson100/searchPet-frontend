f9452f8b330ac30c3b1fde4ff6fd22eb2d0c6bb9
jerson100 <juamkoo@gmail.com>
2022-11-08 15:57:41 -0500

    Obtenemos todas las mascotas perdidas del usuario en la vista del perfil del mismo

--- src/views/public/UserProfileView/views/UserLostPets/UserLostPets.jsx
+++ src/views/public/UserProfileView/views/UserLostPets/UserLostPets.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import JeSection from "../../../../../components/common/JeSection";
+import useAxios from "axios-hooks";
+import LostPetList from "../../../../../components/common/LostPetList/LostPetList";
+import { useOutletContext } from "react-router-dom";
 
 const UserLostPets = () => {
+  const { idUser } = useOutletContext();
+  const [{ loading, data, error }] = useAxios({
+    url: `users/${idUser}/lostpets`,
+  });
   return (
     <>
       <JeSection.Title textAlign="left" variant="h5">
         Mascotas Perdidas
       </JeSection.Title>
+      <LostPetList items={data} loading={loading} />
     </>
   );
 };
