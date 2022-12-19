e1f40f3cf2844710a92d38ac8b2b9a7cad509803
jerson100 <juamkoo@gmail.com>
2022-12-19 11:01:16 -0500

    Realizamos la nueva implementación del nuevo componente de contraseña(input)

--- src/views/public/RegisterView/components/RegisterForm/RegisterForm.jsx
+++ src/views/public/RegisterView/components/RegisterForm/RegisterForm.jsx
@@ -14,149 +14,104 @@ import Person from "@mui/icons-material/Person";
 // import { validateSchema } from "../../../../../utils/validateSchema";
 import { ValidateUserCreationSchema } from "../../../../../api/users.validate";
 import JeInputTextError from "../../../../../components/common/JeInputTextError";
-import Location from "../../../../../components/common/Location";
+import BottomForm from "../BottomForm";
+import JePasswordInput from "../../../../../components/common/JePasswordInput/JePasswordInput";
 
-const RegisterForm = () => {
-  const [location, setlocation] = useState();
+const RegisterForm = React.memo(
+  ({ sendNewUser, loading: loadingCreateUser }) => {
+    return (
+      <Formik
+        initialValues={{
+          name: "",
+          paternalSurname: "",
+          maternalSurname: "",
+          username: "",
+          email: "",
+          password: "",
+          cPassword: "",
+        }}
+        validationSchema={ValidateUserCreationSchema}
+        onSubmit={async (values, { resetForm }) => {
+          await sendNewUser(values, resetForm);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Box component={Form} margin="normal">
+            <JeInputTextError
+              fullWidth
+              name="name"
+              error={!!touched.name && !!errors.name}
+              Icon={People}
+              inputLabel="Nombres"
+            />
 
-  const [{ loading: loadingCreateUser }, createUser] = useAxios(
-    {
-      method: "POST",
-    },
-    { manual: true }
-  );
-  const { enqueueSnackbar } = useSnackbar();
+            <JeInputTextError
+              fullWidth
+              name="paternalSurname"
+              error={!!touched.paternalSurname && !!errors.paternalSurname}
+              Icon={People}
+              inputLabel="Apellido Paterno"
+            />
 
-  const sendNewUser = async (values, resetForm) => {
-    if (location) {
-      try {
-        const a = await createUser({
-          url: "/users",
-          data: {
-            ...values,
-            location: {
-              latitud: location[0],
-              longitud: location[1],
-            },
-          },
-        });
-        setlocation(null);
-        resetForm();
-        enqueueSnackbar(`Se creó la cuenta con gmail ${a.data.email}`, {
-          variant: "success",
-        });
-      } catch (e) {
-        if (e.status) {
-          enqueueSnackbar(e.message, {
-            variant: "error",
-          });
-        }
-      }
-    } else {
-      enqueueSnackbar("Le faltó seleccionar la localización", {
-        variant: "error",
-      });
-    }
-  };
+            <JeInputTextError
+              fullWidth
+              name="maternalSurname"
+              error={!!touched.maternalSurname && !!errors.maternalSurname}
+              Icon={People}
+              inputLabel="Apellido Materno"
+            />
 
-  return (
-    <Formik
-      initialValues={{
-        name: "",
-        paternalSurname: "",
-        maternalSurname: "",
-        username: "",
-        email: "",
-        password: "",
-        cPassword: "",
-      }}
-      validationSchema={ValidateUserCreationSchema}
-      onSubmit={async (values, { resetForm }) => {
-        await sendNewUser(values, resetForm);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Box component={Form} margin="normal">
-          <Grid container>
-            <Grid item xs={12} md={6} lg={5}>
-              <JeInputTextError
-                fullWidth
-                name="name"
-                error={!!touched.name && !!errors.name}
-                Icon={People}
-                inputLabel="Nombres"
-              />
+            <JeInputTextError
+              fullWidth
+              name="username"
+              error={!!touched.username && !!errors.username}
+              Icon={Person}
+              inputLabel="Nombre de usuario"
+            />
 
-              <JeInputTextError
-                fullWidth
-                name="paternalSurname"
-                error={!!touched.paternalSurname && !!errors.paternalSurname}
-                Icon={People}
-                inputLabel="Apellido Paterno"
-              />
+            <JeInputTextError
+              fullWidth
+              name="email"
+              error={!!touched.email && !!errors.email}
+              Icon={Email}
+              inputLabel="email"
+            />
 
-              <JeInputTextError
-                fullWidth
-                name="maternalSurname"
-                error={!!touched.maternalSurname && !!errors.maternalSurname}
-                Icon={People}
-                inputLabel="Apellido Materno"
-              />
+            <JePasswordInput
+              fullWidth
+              type="password"
+              name="password"
+              error={!!touched.password && !!errors.password}
+              Icon={Visibility}
+              inputLabel="Contraseña"
+              autoComplete="new-password"
+            />
 
-              <JeInputTextError
-                fullWidth
-                name="username"
-                error={!!touched.username && !!errors.username}
-                Icon={Person}
-                inputLabel="Nombre de usuario"
-              />
-
-              <JeInputTextError
-                fullWidth
-                name="email"
-                error={!!touched.email && !!errors.email}
-                Icon={Email}
-                inputLabel="email"
-              />
-
-              <JeInputTextError
-                fullWidth
-                type="password"
-                name="password"
-                error={!!touched.password && !!errors.password}
-                Icon={Visibility}
-                inputLabel="Contraseña"
-                autoComplete="new-password"
-              />
-
-              <JeInputTextError
-                fullWidth
-                type="password"
-                name="cPassword"
-                error={!!touched.cPassword && !!errors.cPassword}
-                Icon={Visibility}
-                inputLabel="Confirmar contraseña"
-              />
-            </Grid>
-            <Grid item xs={12} md={6} lg={7} paddingLeft={{ md: "2rem" }}>
-              <Location location={location} setlocation={setlocation} />
-            </Grid>
-          </Grid>
-          <Box display={"flex"} justifyContent="flex-end" marginBottom={"2rem"}>
-            <LoadingButton
-              type="submit"
-              variant="contained"
-              loading={loadingCreateUser}
-              startIcon={<SaveIcon />}
-              loadingPosition="start"
-            >
-              Crear cuenta
-            </LoadingButton>
+            <JePasswordInput
+              fullWidth
+              type="password"
+              name="cPassword"
+              error={!!touched.cPassword && !!errors.cPassword}
+              Icon={Visibility}
+              inputLabel="Confirmar contraseña"
+            />
+            <Box display={"flex"} justifyContent="center" mb={2} mt={2}>
+              <LoadingButton
+                type="submit"
+                variant="contained"
+                loading={loadingCreateUser}
+                startIcon={<SaveIcon />}
+                loadingPosition="start"
+              >
+                Crear cuenta
+              </LoadingButton>
+            </Box>
+            <BottomForm />
           </Box>
-        </Box>
-      )}
-    </Formik>
-  );
-};
+        )}
+      </Formik>
+    );
+  }
+);
 
 export default RegisterForm;
