2354857bb577e86d9c1c2f670ee475a1e16be2bd
jerson100 <juamkoo@gmail.com>
2023-03-08 19:08:55 -0500

    Implementado el diseño para cuando no hay ningún chat seleccionado

--- src/components/common/Chat/components/ChatRight/ChatRight.jsx
+++ src/components/common/Chat/components/ChatRight/ChatRight.jsx
@@ -1,8 +1,59 @@
-import { Paper } from "@mui/material";
+import { Avatar, Paper } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import useChatContext from "../../../../../hooks/useChatContext";
+import NoSelectedChat from "../NoSelectedChat";
 
 const ChatRight = () => {
-  return <Paper></Paper>;
+  const { currentChat } = useChatContext();
+  return (
+    <Paper>
+      {!currentChat ? (
+        <NoSelectedChat />
+      ) : (
+        <>
+          <Header currentChat={currentChat} />
+        </>
+      )}
+    </Paper>
+  );
+};
+
+const Header = ({ currentChat }) => {
+  const { name, urlImageProfile, email } = currentChat;
+  return (
+    <Box
+      sx={{
+        height: "64px",
+        display: "grid",
+        gridTemplateColumns: "auto 1fr",
+        gap: "1rem",
+        p: 2,
+      }}
+    >
+      <Avatar
+        alt={name}
+        src={urlImageProfile}
+        sx={{ width: { xs: 40, md: 55 }, height: { xs: 40, md: 55 } }}
+      />
+      <Box>
+        <Typography
+          variant="body2"
+          sx={{ marginBottom: ".25rem" }}
+          fontWeight="bold"
+        >
+          {name}
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ marginBottom: ".25rem" }}
+          fontWeight="bold"
+        >
+          {email}
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default ChatRight;
---
+++ src/components/common/Chat/components/NoSelectedChat/NoSelectedChat.jsx
@@ -0,0 +1,21 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const NoSelectedChat = () => {
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="body2" component="p">
+        Nigún chat seleccionado
+      </Typography>
+    </Box>
+  );
+};
+
+export default NoSelectedChat;
