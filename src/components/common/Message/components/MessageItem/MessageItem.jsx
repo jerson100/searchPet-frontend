aba6a12dbdf16d2b197da93f6e99722f9758f740
jerson100 <juamkoo@gmail.com>
2023-03-20 09:53:45 -0500

    Se formatea la fecha de creación del mensaje

--- src/components/common/Message/components/MessageItem/MessageItem.jsx
+++ src/components/common/Message/components/MessageItem/MessageItem.jsx
@@ -1,98 +1,107 @@
 import { Avatar, Box, Skeleton, Typography } from "@mui/material";
-import React from "react";
+import React, { useMemo } from "react";
+import { getTweetPublicationDate } from "../../../../../utils/date";
 
-const MessageItem = ({
-  room,
-  _id,
-  sender,
-  createdAt,
-  updatedAt,
-  text,
-  image,
-  cords,
-  seen,
-  isMyMessage = false,
-  loading = false,
-}) => {
-  return (
-    <Box
-      component="li"
-      sx={{
-        display: "flex",
-        justifyContent: isMyMessage ? "flex-end" : "flex-start",
-      }}
-      pl={2}
-      pr={2}
-      mb={2}
-    >
+const MessageItem = React.memo(
+  ({
+    room,
+    _id,
+    sender,
+    createdAt,
+    updatedAt,
+    text,
+    image,
+    cords,
+    seen,
+    isMyMessage = false,
+    loading = false,
+  }) => {
+    const formatedDate = useMemo(() => {
+      return getTweetPublicationDate(new Date(createdAt));
+    }, [createdAt]);
+
+    return (
       <Box
+        component="li"
         sx={{
           display: "flex",
-          flexDirection: isMyMessage ? "row" : "row-reverse",
+          justifyContent: isMyMessage ? "flex-end" : "flex-start",
         }}
-        gap={1}
+        pl={2}
+        pr={2}
+        mb={2}
       >
         <Box
           sx={{
-            display: "Flex",
-            flexDirection: "column",
-            alignItems: isMyMessage ? "flex-end" : "flex-start",
+            display: "flex",
+            flexDirection: isMyMessage ? "row" : "row-reverse",
           }}
+          gap={1}
         >
+          <Box
+            sx={{
+              display: "Flex",
+              flexDirection: "column",
+              alignItems: isMyMessage ? "flex-end" : "flex-start",
+            }}
+          >
+            {loading ? (
+              <>
+                <Skeleton
+                  variant="text"
+                  sx={{ width: "100px", height: "20px", mb: 1 }}
+                />
+                <Skeleton
+                  variant="text"
+                  sx={{ width: "40px", height: "20px", mb: 1 }}
+                />
+              </>
+            ) : (
+              <>
+                <Typography
+                  variant="body2"
+                  component="p"
+                  sx={{
+                    maxWidth: "150px",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                    background: isMyMessage
+                      ? "#1565c033"
+                      : "rgba(0, 0, 0,0.21)",
+                    padding: "0.5rem 1rem",
+                    borderRadius: "8px",
+                  }}
+                >
+                  {text}
+                </Typography>
+                <Typography
+                  variant="caption"
+                  component="p"
+                  color="text.secondary"
+                >
+                  {formatedDate}
+                </Typography>
+              </>
+            )}
+          </Box>
           {loading ? (
-            <>
-              <Skeleton
-                variant="text"
-                sx={{ width: "100px", height: "20px", mb: 1 }}
-              />
-              <Skeleton
-                variant="text"
-                sx={{ width: "40px", height: "20px", mb: 1 }}
-              />
-            </>
+            <Skeleton
+              variant="circular"
+              sx={{ width: { xs: 35, md: 35 }, height: { xs: 35, md: 35 } }}
+            />
           ) : (
-            <>
-              <Typography
-                variant="body2"
-                component="p"
-                sx={{
-                  maxWidth: "150px",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
-                  background: isMyMessage ? "#1565c033" : "rgba(0, 0, 0,0.21)",
-                  padding: "0.5rem 1rem",
-                  borderRadius: "8px",
-                }}
-              >
-                {text}
-              </Typography>
-              <Typography
-                variant="caption"
-                component="p"
-                color="text.secondary"
-              >
-                1h
-              </Typography>
-            </>
+            <Avatar
+              alt={sender.name}
+              src={sender.urlImageProfile}
+              sx={{ width: { xs: 35, md: 35 }, height: { xs: 35, md: 35 } }}
+            />
           )}
         </Box>
-        {loading ? (
-          <Skeleton
-            variant="circular"
-            sx={{ width: { xs: 35, md: 35 }, height: { xs: 35, md: 35 } }}
-          />
-        ) : (
-          <Avatar
-            alt={sender.name}
-            src={sender.urlImageProfile}
-            sx={{ width: { xs: 35, md: 35 }, height: { xs: 35, md: 35 } }}
-          />
-        )}
       </Box>
-    </Box>
-  );
-};
+    );
+  }
+);
 
 const Loading = ({ isMyMessage }) => {
   return (
